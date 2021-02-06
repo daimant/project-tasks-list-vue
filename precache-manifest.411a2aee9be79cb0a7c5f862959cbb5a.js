fbdf4a57d7396d9421978b7f66061c481d1f0060
Sergey Pomortsev <zaooooza@gmail.com>
2021-02-06 18:00:00 +0300

    Updates

---
+++ precache-manifest.411a2aee9be79cb0a7c5f862959cbb5a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5d447e47791b0ff0c24",
+    "url": "/css/app.e593cbe9.css"
+  },
+  {
+    "revision": "1c4d48ea68c86f9a8924",
+    "url": "/css/chunk-02cceba3.d2a5595f.css"
+  },
+  {
+    "revision": "727d0e4ff03f2333e282",
+    "url": "/css/chunk-vendors.24ce4a6c.css"
+  },
+  {
+    "revision": "307b1470eb8e47a6542ae37f698b9ce0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5d447e47791b0ff0c24",
+    "url": "/js/app.6ca20abc.js"
+  },
+  {
+    "revision": "1c4d48ea68c86f9a8924",
+    "url": "/js/chunk-02cceba3.92036bd7.js"
+  },
+  {
+    "revision": "d8e3b01d5f081f69b390",
+    "url": "/js/chunk-2d0b64c8.c923f6da.js"
+  },
+  {
+    "revision": "ec7e28c616bb7d2de310",
+    "url": "/js/chunk-2d21ef2c.36b17440.js"
+  },
+  {
+    "revision": "727d0e4ff03f2333e282",
+    "url": "/js/chunk-vendors.55d46480.js"
+  },
+  {
+    "revision": "20c2b84be373721ea7dc0f915648ac97",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
